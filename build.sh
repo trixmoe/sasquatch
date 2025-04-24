138db529f3a7e823f14514df4d4b96116226d71b
Peter Stenger <peter.a.stenger@gmail.com>
2025-04-24 12:28:20 +0200

    Fixup to https://github.com/devttys0/sasquatch/issues/58

--- build.sh
+++ build.sh
@@ -31,7 +31,28 @@ rm -rf squashfs4.3
 tar -zxvf squashfs4.3.tar.gz
 
 # Patch, build, and install the source
-cd squashfs4.3
+
+cd squashfs4.3/squashfs-tools/
+
+sed -i.orig 's/FNM_EXTMATCH/0/; s/sysinfo.h/sysctl.h/; s/^inline/static inline/' mksquashfs.c unsquashfs.c
+
+cat <<END >> xattr.h
+#define llistxattr(path, list, size) \
+  (listxattr(path, list, size, XATTR_NOFOLLOW))
+#define lgetxattr(path, name, value, size) \
+  (getxattr(path, name, value, size, 0, XATTR_NOFOLLOW))
+#define lsetxattr(path, name, value, size, flags) \
+  (setxattr(path, name, value, size, 0, flags | XATTR_NOFOLLOW))
+END
+
+cd ..
+
+patch -p1 < ../mac.patch
 patch -p0 < ../patches/patch0.txt
+
 cd squashfs-tools
+sed -i.orig 's/\-Werror/\-Werror \-Wno\-self\-assign/' Makefile
+sed -i.orig 's/#LZO_DIR \= .*/LZO_DIR \= \/opt\/homebrew\/opt\/lzo/' Makefile
+sed -i.orig 's/#LZMA_XZ_SUPPORT \= 1/EXTRA_CFLAGS \= \-I\/opt\/homebrew\/opt\/xz\/include/' Makefile
+
 make
